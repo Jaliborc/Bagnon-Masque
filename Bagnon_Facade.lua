fc430e621dc9072da88177c017e3fed062b9d28c
Joao Cardoso <contact@jaliborc.com>
2017-01-30 15:59:21 +0000

    no message

--- Bagnon_Facade.lua
+++ Bagnon_Facade.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2011-2013 João Cardoso
+Copyright 2011-2016 João Cardoso
 Bagnon Facade is distributed under the terms of the GNU General Public License (or the Lesser GPL).
 This file is part of Bagnon Facade.
 
