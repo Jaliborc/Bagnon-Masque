8068010b73d771cda11c2bc9e3f69abb4357f025
Joao Cardoso <jaliborc@arenajunkies.com>
2020-01-18 02:33:55 +0100

    Version 8.3.0

--- Bagnon_Facade.lua
+++ Bagnon_Facade.lua
@@ -1,69 +1,75 @@
 --[[
-Copyright 2011-2017 João Cardoso
-Bagnon Facade is distributed under the terms of the GNU General Public License (Version 3).
-As a special exception, the copyright holders of this addon do not give permission to
-redistribute and/or modify it.
+Copyright 2011-2020 João Cardoso
+Bagnon Facade is distributed under the terms of the GNU General Public License (or the Lesser GPL).
+This file is part of Bagnon Facade.
 
-This addon is distributed in the hope that it will be useful,
+Bagnon Facade is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bagnon Facade is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with the addon. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
-
-This file is part of Bagnon Facade.
+along with Bagnon Facade. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
+local Addon = Bagnon or Combuctor
 local Masque = LibStub('Masque')
+
 for i, frameID in ipairs {'inventory', 'bank', 'guildbank', 'voidstorage'} do
-	Masque:Group('Bagnon', frameID .. ' - items')
-	Masque:Group('Bagnon', frameID .. ' - bags')
+	Masque:Group(Addon.Name, frameID .. ' - items')
+	Masque:Group(Addon.Name, frameID .. ' - bags')
 end
 
-local Item, Bag = Bagnon.ItemSlot, Bagnon.Bag
-local NewItem, FreeItem = Item.New, Item.Free
+
+--[[ Overrides ]]--
+
+local Item, Bag = Addon.Item, Addon.Bag
+local NewItem, ReleaseItem = Item.New, Item.Release
 local NewBag = Bag.New
 
 function Item:New(...)
-	local button = NewItem(self, ...)
-	local name = button:GetName()
+	local b = NewItem(self, ...)
+	local name = b:GetName()
 
-	Masque:Group('Bagnon', button:GetFrameID() .. ' - items'):AddButton(button, {
-		Count = button.Count or _G[name .. 'Count'],
-		Icon = button.icon or _G[name .. 'IconTexture'],
-
-		Normal = button:GetNormalTexture(),
-		Highlight = button:GetHighlightTexture(),
-		Pushed = button:GetPushedTexture(),
-
-		Cooldown = button.Cooldown,
-		Border = button.Border,
+	Masque:Group(Addon.Name, b:GetFrameID() .. ' - items'):AddButton(b, {
+		Count = b.Count or _G[name .. 'Count'],
+		Icon = b.icon or _G[name .. 'IconTexture'],
+		Normal = b:GetNormalTexture(),
+		Pushed = b:GetPushedTexture(),
+		Highlight = b:GetHighlightTexture(),
+		Cooldown = b.Cooldown,
+		Border = b.IconGlow,
 	})
 
-	return button
+	b.IconBorder:SetAlpha(0)
+	return b
 end
 
-function Item:Free()
+function Item:Release()
 	if self:GetFrameID() then
-		Masque:Group('Bagnon', self:GetFrameID() .. ' - items'):RemoveButton(self)
+		Masque:Group(Addon.Name, self:GetFrameID() .. ' - items'):RemoveButton(self)
 	end
 
-	FreeItem(self)
+	ReleaseItem(self)
 end
 
 function Bag:New(...)
-	local button = NewBag(self, ...)
+	local b = NewBag(self, ...)
 
-	Masque:Group('Bagnon', button:GetFrameID() .. ' - bags'):AddButton(button, {
-		Count = button.Count,
-		Icon = button.Icon,
+	Masque:Group(Addon.Name, b:GetFrameID() .. ' - bags'):AddButton(b, {
+		Count = b.Count,
+		Icon = b.Icon,
 
-		Normal = button:GetNormalTexture(),
-		Highlight = button:GetHighlightTexture(),
-		Pushed = button:GetPushedTexture(),
-		Checked = button:GetCheckedTexture(),
+		Normal = b:GetNormalTexture(),
+		Highlight = b:GetHighlightTexture(),
+		Pushed = b:GetPushedTexture(),
+		Checked = b:GetCheckedTexture(),
 	})
 
-	return button
+	return b
 end
--- Changelog.md
+++ Changelog.md
@@ -1,59 +1,74 @@
-##### 18
+#### 8.3.0
+* Updated for Visions of Nzoth.
+
+##### 8.2.1
+* Updated for Bagnon rework.
+
+##### 8.2.0
+* Updated for World of Warcraft patch 8.2.0.
+
+##### 8.1.0
+* Updated for World of Warcraft patch 8.1.0.
+
+#### 19
+* Updated for Battle for Azeroth
+
+#### 18
 * Updated for Shadow of Argus
 
-##### 17
+#### 17
 * Updated for Bagnon 7.2 onwards
 * Now can skin bag buttons as well (can be configured separately)
 
-##### 16
+#### 16
 * Updated for Return to Kharazan
 
-##### 15
+#### 15
 * Updated for Fury of Hellfire
 
-##### 14
+#### 14
 * Updated for Bagnon 6.1
 
-##### 13
+#### 13
 * Updated for latest Bagnon version.
 
-##### 12
+#### 12
 * Fixed yet another major bug.
 
-##### 11
+#### 11
 * Fixed major bug.
 
-##### 10
+#### 10
 * Updated for The Iron Tide.
 
-##### 9
+#### 9
 * Updated for Siege of Ogrimmar.
 
-##### 8
+#### 8
 * Updated for patch 5.3: Escalation.
 
-##### 7
+#### 7
 * Updated for patch 5.2: The Thunder King
 
 ##### 6.1
 * Removed unnecessary files increasing download space.
 
-##### 6
+#### 6
 * Updated for patch 5.1: Landfall!
 
-##### 5
+#### 5
 * Updated for Mists of Pandaria
 
-##### 4
-* Now uses the new Masque API,  making it much more stable and have smaller download
+#### 4
+* Now uses the new Masque API, making it much more stable and have smaller download
 
-##### 3
+#### 3
 * Updated for WoW 4.3
 * Now should work with the guild bank
 
-##### 2
+#### 2
 * Should now work when Masque and other addons that support ButtonFacade are enabled (although the problem is not from my end)
 * Still does not work with the guild bank
 
-##### 1
+#### 1
 * Initial release
